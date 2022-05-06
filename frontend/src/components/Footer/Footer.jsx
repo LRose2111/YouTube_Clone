0c27c9504f51a610d581ebd0081e5995cf187391
Lakota Rose <itsrosebro@gmail.com>
2022-05-06 10:37:27 -0500

    Changed Footer/Nav startercode.

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,12 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright © 2022</p>{" "}
+      <div className="copy-right">
+            <medium className='copy-color'> FRM ABVE YT CLONE &copy;{new Date().getFullYear()}</medium> <b>| HUNTR | NEW WAVE</b>
+      </div>
+      <nav>
+        <li className = "footer-list"><a href="https://github.com/LRose2111" className = "footer-ref" target="_blank"><medium className = 'footer-color'>My GitHub</medium></a></li>
+      </nav>
     </footer>
   );
 };

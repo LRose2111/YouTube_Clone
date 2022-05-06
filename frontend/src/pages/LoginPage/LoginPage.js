080a91d163e5ff59b34686e9cbc97f65ec85147e
Lakota Rose <itsrosebro@gmail.com>
2022-05-06 15:01:35 -0500

    Adding work for LoginPage.

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -19,32 +19,37 @@ const LoginPage = () => {
   }, [isServerError]);
 
   return (
-    <div className="container">
-      <form className="form" onSubmit={handleSubmit}>
-        <label>
-          Username:{" "}
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Password:{" "}
-          <input
-            type="text"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </label>
-        {isServerError ? (
-          <p className="error">Login failed, incorrect credentials!</p>
-        ) : null}
-        <Link to="/register">Click to register!</Link>
-        <button>Login!</button>
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <div className= "login-contain">
+          <div>
+            <label className="login-label">Username:{" "}</label>
+            <input type="text" name="username" value={formData.username} onChange={handleInputChange}/>
+          </div>
+          <div>
+            <label className="login-label">Password:{" "}</label>
+              <input type="text" name="password" value={formData.password} onChange={handleInputChange}/>
+            {isServerError ? (
+              <p className="error">Login failed, incorrect credentials!</p>
+            ) : null}
+          </div>
+        </div>
+        <div className="login-button-contain">
+          <div>
+            <button className="login-button">Login</button>
+          </div>
+        </div>
+        <div className="login-register">
+          <Link to="/register" ><b>Click to Register</b></Link>
+        </div>
       </form>
+      <div className="login-notice"><p><medium className='log-reg-log'>Login</medium> to see more video content & options.</p></div>
+      <div className="iframe-vid">
+        <iframe id="ytplayer" type="text/html" width="600" height="320"
+        src="https://www.youtube.com/embed/D-hKu4BT4U0"
+        frameborder="0"></iframe>
+      </div>
+      <div><p><b>Click to Watch</b> (no need to login for this one)</p></div>
     </div>
   );
 };
